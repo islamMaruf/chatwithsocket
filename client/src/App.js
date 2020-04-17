4dac91fd43e8b2b7697bbe71ce7db1b2c6f8e3c3
islamMaruf <marufislam7424@gmail.com>
2020-04-17 08:35:52 +0600

    project initialize for chat app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path={ } component={}/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
